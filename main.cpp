dd5b3712bb9d48c860ee339635eeb31ed6f19d9d
RubenMR99 <ruben.moreno.romero@gmail.com>
2018-06-10 15:19:15 +0200

    Primera penja, programa buit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/***** ESPECIFICACIÓ
+AUTOR: RUBEN MORENO ROMERO
+PROBLEMA: Entrant dades de persones i de premis per teclat fer un concurs.
+ENTRADA: Dades personals (Nom/NºDorsal) i Dades dels premis (Nom del premi - Numero del premi)
+SORTIDA: Surt per pantalla qui ha guanyat que.
+VERSIÓ: 0.1 10/06/18
+*****/
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#ifdef _WIN32
+    #include<windows.h>
+#endif // _WIN32
+using namespace std;
+
+int main() {
+    #ifdef _WIN32
+    SetConsoleOutputCP(1252);
+    SetConsoleCP(1252);
+    #endif
+
+    cout<<"Hólâüàé";
+    return 0;
+}
